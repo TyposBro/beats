14a3d39b716589065b8d543240046bad339a2e35
Azizbek <khasanjonovich@gmail.com>
2023-04-26 16:32:52 +0900

    aimp almost ready

--- src/pages/RawAudio.jsx
+++ src/pages/RawAudio.jsx
@@ -1,4 +1,6 @@
-// import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import WaveSurfer from "wavesurfer.js";
+
 import {
   AiOutlineMenu,
   AiOutlineHeart,
@@ -16,6 +18,28 @@ import {
   MdOutlineChevronLeft,
 } from "react-icons/md";
 
+const brand = "rgba(0, 229, 139, 1)";
+
+const getOptions = (ref, plugins = []) => {
+  return {
+    container: ref,
+    waveColor: "rgba(250, 239, 221, 0.5)",
+    progressColor: brand,
+    cursorColor: brand,
+    barWidth: 3,
+    barRadius: 3,
+    responsive: true,
+    interact: true,
+    height: 40,
+    barHeight: 0.6,
+    // If true, normalize by the maximum peak instead of 1.0.
+    normalize: true,
+    // Use the PeakCache to improve rendering speed of large waveforms.
+    partialRender: true,
+    plugins: plugins,
+  };
+};
+
 // const options = {
 //   "Deep Sleep(2)": 2,
 //   "Relaxed(8)": 8,
@@ -26,53 +50,85 @@ import {
 // };
 
 const Binaurial = () => {
-  // const [input, setInput] = useState(110);
-  // const [diff, setDiff] = useState(10);
-  // const [playing, setPlaying] = useState(false);
-  // useEffect(() => {
-  //   console.log(diff);
-  //   if (playing) start();
-  // }, [diff]);
-  // const onChange = (e) => {
-  //   let text = e.target.value.replace(/[^0-9.]/g, "").replace(/(\..*?)\..*/g, "$1");
-  //   setInput(text);
-  //   if (channel1.current) {
-  //     channel1.current.suspend();
-  //     channel2.current.suspend();
-  //   }
-  // };
-  // const channel1 = useRef();
-  // const channel2 = useRef();
+  const [input, setInput] = useState(110);
+  const [diff, setDiff] = useState(10);
+  const [blob, setBlob] = useState("/audio.mp3");
 
-  // const start = () => {
-  //   if (playing) stop();
-  //   channel1.current = new (window.AudioContext || window.webkitAudioContent)();
-  //   channel2.current = new (window.AudioContext || window.webkitAudioContent)();
+  const channel1 = useRef();
+  const channel2 = useRef();
 
-  //   const osc1 = channel1.current.createOscillator(); //default frequency is 440HZ
-  //   osc1.frequency.value = input;
-  //   const osc2 = channel2.current.createOscillator();
-  //   osc2.frequency.value = input - diff;
+  const waveformRef = useRef(null);
+  const wavesurfer = useRef(null);
 
-  //   const leftStereo = new StereoPannerNode(channel1.current);
-  //   leftStereo.pan.value = -1;
-  //   const rightStereo = new StereoPannerNode(channel2.current, { pan: 1 });
+  const [play, setPlay] = useState(false);
 
-  //   osc1.connect(leftStereo).connect(channel1.current.destination);
+  // create new WaveSurfer instance
+  // On component mount and when url changes
+  useEffect(() => {
+    const options = getOptions(waveformRef.current);
+    wavesurfer.current = WaveSurfer.create(options);
+    wavesurfer.current.load(blob);
 
-  //   osc2.connect(rightStereo).connect(channel2.current.destination);
+    wavesurfer.current.on("ready", () => {
+      setPlay(false);
+      wavesurfer.current.setVolume(1);
+    });
+    // Removes events, elements and disconnects Web Audio nodes.
+    // when component unmount
+    return () => wavesurfer.current.destroy();
+  }, [blob]);
 
-  //   //Play
-  //   osc1.start();
-  //   osc2.start();
+  const handlePlayPause = () => {
+    if (!play) {
+      wavesurfer.current.playPause();
+      setPlay(true);
+    } else {
+      setPlay(false);
+      wavesurfer.current.playPause();
+    }
+  };
 
-  //   setPlaying(true);
-  // };
-  // const stop = () => {
-  //   channel1.current.suspend();
-  //   channel2.current.suspend();
-  //   setPlaying(false);
-  // };
+  useEffect(() => {
+    console.log(diff);
+    if (play) start();
+  }, [diff]);
+
+  const onChange = (e) => {
+    let text = e.target.value.replace(/[^0-9.]/g, "").replace(/(\..*?)\..*/g, "$1");
+    setInput(text);
+    if (channel1.current) {
+      channel1.current.suspend();
+      channel2.current.suspend();
+    }
+  };
+
+  const start = () => {
+    if (play) stop();
+    channel1.current = new (window.AudioContext || window.webkitAudioContent)();
+    channel2.current = new (window.AudioContext || window.webkitAudioContent)();
+
+    const osc1 = channel1.current.createOscillator(); //default frequency is 440HZ
+    osc1.frequency.value = input;
+    const osc2 = channel2.current.createOscillator();
+    osc2.frequency.value = input - diff;
+
+    const leftStereo = new StereoPannerNode(channel1.current, { pan: -1 });
+    const rightStereo = new StereoPannerNode(channel2.current, { pan: 1 });
+
+    osc1.connect(leftStereo).connect(channel1.current.destination);
+    osc2.connect(rightStereo).connect(channel2.current.destination);
+
+    //Play
+    osc1.start();
+    osc2.start();
+
+    setPlay(true);
+  };
+  const stop = () => {
+    channel1.current.suspend();
+    channel2.current.suspend();
+    setPlay(false);
+  };
   return (
     <div className="w-screen h-screen bg-white flex flex-col">
       <div className="grow-[3] m-2">
@@ -82,16 +138,16 @@ const Binaurial = () => {
           <ImEqualizer2 className="w-12 h-12 p-2 cursor-pointer basis-16" />
           <AiOutlineClockCircle className="w-12 h-12 p-2 cursor-pointer basis-16" />
         </div>
-        <div className="mx-8 my-2 rounded-md overflow-hidden relative">
+        <div className="mx-8 my-2 max-h-80 rounded-md overflow-hidden relative">
           <img
             src="https://s3-aop.plusmember.jp/prod/public/yorushika/contents/discography/a2c91653ae3e7272ee7847002a9fb95f.jpeg"
             alt="Album Cover"
             className="object-contain"
           />
           <div className="absolute flex flex-col w-fit bottom-1 left-1 text-neutral-300">
-            <span className="">Yorushika</span>
-            <span>Moonlight</span>
-            <span>Miyagochi</span>
+            <div className="">Yorushika</div>
+            <div>Moonlight</div>
+            <div>Miyagochi</div>
           </div>
         </div>
         <div className="flex justify-between px-2 items-center text-lg">
@@ -103,14 +159,33 @@ const Binaurial = () => {
           </div>
           <AiOutlineMore className="h-12 w-8" />
         </div>
+        <div className="p-2">
+          <div id="waveform" ref={waveformRef} />
+        </div>
       </div>
       <div className="flex flex-col w-full basis-24 bg-yellow-600 grow">
         <div className=" flex justify-between items-center">
           <MdShuffle className="grow h-16 p-2 text-amber-300" />
           <div className="flex justify-between grow-[3] items-center">
-            <MdOutlineChevronLeft className="align-start grow h-24 text-gray-300" />
-            <AiOutlinePlayCircle className="align-start grow h-24 text-gray-300" />
-            <MdOutlineChevronRight className="align-start grow h-24 text-gray-300" />
+            <MdOutlineChevronLeft
+              className="align-start grow h-24 text-gray-300"
+              onClick={() => wavesurfer.current.skip(-10)}
+            />
+            {play ? (
+              <AiOutlinePauseCircle
+                className="align-start grow h-24 text-gray-300"
+                onClick={handlePlayPause}
+              />
+            ) : (
+              <AiOutlinePlayCircle
+                className="align-start grow h-24 text-gray-300"
+                onClick={handlePlayPause}
+              />
+            )}
+            <MdOutlineChevronRight
+              className="align-start grow h-24 text-gray-300"
+              onClick={() => wavesurfer.current.skip(10)}
+            />
           </div>
           <MdOutlineRepeat className="grow h-16 p-2 text-amber-300" />
         </div>
